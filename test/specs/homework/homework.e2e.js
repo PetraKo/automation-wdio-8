4aee1730974739ce930b3a83dcc1627971eb5c6a
PetraKo <koliskova.petra@gmail.com>
2023-04-26 17:54:56 +0200

    updated exercise

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -3,6 +3,12 @@ describe('Homework', async () => {
     it('should open page and create screenshot', async () => {
 
         // sem vypracuj domácí úkol
+
+        await browser.reloadSession();
+
+        await browser.url('/prihlaseni');
+
+        await browser.pause(5000);
         
 
     });
